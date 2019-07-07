f1605ea23b78447782d488260594472d2476af3f
laybelmeyden <trycollens@gmail.com>
2019-07-07 23:29:58 +0300

    mail/smtp5

--- app/Form.php
+++ app/Form.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Form extends Model
 {
-
+    protected $fillable = [
+    'email',
+    'p1',
+    'p2',
+    'p3',
+    'p4',
+    'p5',
+    'p6',
+    'p7',
+    'p8',
+    'p9',
+    'p11',
+    ];
 }
--- app/Formtwo.php
+++ app/Formtwo.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Formtwo extends Model
 {
-  
+    protected $fillable = [
+    'email',
+    'p1' ,
+    'p2' ,
+    'p3',
+    'p4' ,
+    'p5' ,
+    'p6' ,
+    'p7' ,
+    'p8' ,
+    ];
 }
