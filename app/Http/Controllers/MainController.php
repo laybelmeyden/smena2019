31171e4bdfe8a50e722673383b5eb413985f69ac
laybelmeyden <trycollens@gmail.com>
2019-07-07 23:38:29 +0300

    mail/smtp7

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -69,38 +69,39 @@ class MainController extends Controller
       
       public function form1(Request $request)
       {
+        Form::create([
+      'name' => request('name'),
+      'date' => request('date'),
+      'birthplace' => request('birthplace'),
+      'address' => request('address'),
+      'place' => request('place'),
+      'exp' => request('exp'),
+      'wins' => request('wins'),
+      'letter' => request('letter'),
+      'tel' => request('tel'),
+      'email' => request('email'),
+      'app' => request('app')
+      ]
+      );
 
       $data= array(
-      'email' => request('email'),
-      'p1' => request('p1'),
-      'p2' => request('p2'),
-      'p3' => request('p3'),
-      'p4' => request('p4'),
-      'p5' => request('p5'),
-      'p6' => request('p6'),
-      'p7' => request('p7'),
-      'p8' => request('p8'),
-      'p9' => request('p9'),
-      'p11' => request('p11'),
-      );
-      Form::create([
-        'name' => request('p1'),
-        'date' => request('p2'),
-        'birthplace' => request('p3'),
-        'address' => request('p4'),
-        'place' => request('p5'),
-        'exp' => request('p6'),
-        'wins' => request('p7'),
-        'letter' => request('p8'),
-        'tel' => request('p9'),
+        'name' => request('name'),
+        'date' => request('date'),
+        'birthplace' => request('birthplace'),
+        'address' => request('address'),
+        'place' => request('place'),
+        'exp' => request('exp'),
+        'wins' => request('wins'),
+        'letter' => request('letter'),
+        'tel' => request('tel'),
         'email' => request('email'),
-        'app' => request('p11')]
-        );
+        'app' => request('app')
+      );
 
        \Mail::send('email.mail1', $data, function($message) use ($data)
     {
         $mail_admin = env('MAIL_USERNAME');
-        $message->from($data['email'],$data['p1'],$data['p2'],$data['p3'],$data['p4'],$data['p5'],$data['p6'],$data['p7'],$data['p8'],$data['p9'],$data['p11']);
+        $message->from($data['email'],$data['name'],$data['date'],$data['birthplace'],$data['address'],$data['place'],$data['exp'],$data['wins'],$data['letter'],$data['tel'],$data['app']);
         $message->to($mail_admin, 'For Admin')->subject('Message from site');
      });
      
@@ -112,14 +113,14 @@ class MainController extends Controller
       {
          Formtwo::create([ 
         'email' => request('email'),    
-        'p1' => request('fio'), 
-        'p2' => request('nameproject'), 
-        'p3' => request('celi'), 
-        'p4' => request('aktual'), 
-        'p5' => request('opis'), 
-        'p6' => request('plan'),
-        'p7' => request('team'), 
-        'p8' => request('priloj'), 
+        'fio' => request('p1'), 
+        'nameproject' => request('p2'), 
+        'celi' => request('p3'), 
+        'aktual' => request('p4'), 
+        'opis' => request('p5'), 
+        'plan' => request('p6'),
+        'team' => request('p7'), 
+        'priloj' => request('p8'), 
         ]); 
           
           
--- resources/views/pages/anketa.blade.php
+++ resources/views/pages/anketa.blade.php
@@ -9,23 +9,23 @@
 					<ol class="fs-fields">
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q1">Введите Ваше ФИО</label>
-							<input class="input validate" name="p1" placeholder="Введите ваше имя" required>
+							<input class="input validate" name="name" placeholder="Введите ваше имя" required>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q2">Введите Вашу дату рождения</label>
-							<input class="fs-anim-lower validate" id="q2" name="p2" type="text" placeholder="21 мая 2102" required/>
+							<input class="fs-anim-lower validate" id="q2" name="date" type="text" placeholder="21 мая 2102" required/>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q3">Введите место Вашего рождения</label>
-							<input class="fs-anim-lower validate" id="q3" name="p3" type="text" placeholder="г.Городовой" required/>
+							<input class="fs-anim-lower validate" id="q3" name="birthplace" type="text" placeholder="г.Городовой" required/>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q4">Введите адрес Вашего места жительства</label>
-							<input class="fs-anim-lower validate" id="q4" name="p4" type="text" placeholder="г.Городовой, улица Уличная, дом 23, квартира 1" required/>
+							<input class="fs-anim-lower validate" id="q4" name="address" type="text" placeholder="г.Городовой, улица Уличная, дом 23, квартира 1" required/>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q5">Введите место Вашего обучения</label>
-							<input class="fs-anim-lower validate" id="q5" name="p5" type="text" placeholder="Школа №23" required/>
+							<input class="fs-anim-lower validate" id="q5" name="place" type="text" placeholder="Школа №23" required/>
 						</li>
 						
 						
@@ -33,22 +33,22 @@
 							<label class="fs-field-label fs-anim-upper" for="q6">Наличие опыта участия в Конкурсах и Олимпиадах в сфере информационных технологий, программирования, киберспорта:</label>
 							<label class="fs-field-label fs-anim-upper" for="q6" style="font-size:12px;">(указывается дата участия и наименование Конкурса или Олимпиады)</label>
 							
-							<textarea class="fs-anim-lower" id="q6" name="p6"></textarea>
+							<textarea class="fs-anim-lower" id="q6" name="exp"></textarea>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q7">Наличие / отсутствие наград, призовых мест и иных подтвержденных побед в конкурсах и олимпиадах по программированию, информационным технологиям и киберспорту:</label>
 							<label class="fs-field-label fs-anim-upper" for="q6" style="font-size:12px;">(указывается дата проведения, наименование Конкурса или Олимпиады и призовое место, награда)</label>
 							
-							<textarea class="fs-anim-lower" id="q7" name="p7"></textarea>
+							<textarea class="fs-anim-lower" id="q7" name="wins"></textarea>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper lb2" for="q8">Наличие рекомендательного письма от руководителя образовательного учреждения</label>
 							<label class="fs-field-label fs-anim-upper lb1" for="q8" style="font-size:12px;">(указывается наименование образовательного учреждения, номер и дата письма)</label>
-							<textarea class="fs-anim-lower" id="q8" name="p8"></textarea>
+							<textarea class="fs-anim-lower" id="q8" name="letter"></textarea>
 						</li>
 						<li>
 							<label class="fs-field-label fs-anim-upper" for="q9" >Введите номер Вашего телефона</label>
-							<input class="fs-anim-lower validate"  type="text" id="q9" name="p9" placeholder="+7 (921) 876 32 42" required/>
+							<input class="fs-anim-lower validate"  type="text" id="q9" name="tel" placeholder="+7 (921) 876 32 42" required/>
 						</li>
 						<li>
 							 <!--data-info="Введите свою почту!" -->
@@ -61,7 +61,7 @@
 
 - скан – копии дипломов победителя в Конкурсах и Олимпиадах;)</label>
 							
-							<input type="file" id="q11" name="p11" class="fs-anim-lower">
+							<input type="file" id="q11" name="app" class="fs-anim-lower">
 						</li>
 					</ol>
 					<div class="col l12" style="    text-align: right;
