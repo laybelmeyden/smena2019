2826d38ade6fcb2c7ba4a2448631267afacd40e1
laybelmeyden <trycollens@gmail.com>
2019-07-07 23:42:58 +0300

    mail/smtp8

--- app/Form.php
+++ app/Form.php
@@ -9,15 +9,15 @@ class Form extends Model
 {
     protected $fillable = [
     'email',
-    'p1',
-    'p2',
-    'p3',
-    'p4',
-    'p5',
-    'p6',
-    'p7',
-    'p8',
-    'p9',
-    'p11',
+    'name',
+    'date',
+    'birthplace',
+    'address',
+    'place',
+    'exp',
+    'wins',
+    'letter',
+    'tel',
+    'app',
     ];
 }
