b1520eb3fdd310abfe2071a2d2755092fbaf0937
laybelmeyden <trycollens@gmail.com>
2019-07-07 23:26:58 +0300

    mail/smtp4

--- app/Form.php
+++ app/Form.php
@@ -7,17 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Form extends Model
 {
-    protected $fillable = [
-    'email',
-    'p1',
-    'p2',
-    'p3',
-    'p4',
-    'p5',
-    'p6',
-    'p7',
-    'p8',
-    'p9',
-    'p11',
-    ];
+
 }
